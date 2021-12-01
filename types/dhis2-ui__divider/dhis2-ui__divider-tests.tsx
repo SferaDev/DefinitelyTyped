8b8476dd863f3b2c6e70c40522468c251744eb99
Alexis Rico <sferadev@gmail.com>
2021-12-01 11:17:51 +0100

    Add divider and help
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

---
+++ types/dhis2-ui__divider/dhis2-ui__divider-tests.tsx
@@ -0,0 +1,10 @@
+import { Divider } from '@dhis2-ui/divider';
+import * as React from 'react';
+
+const element = (
+    <>
+        <div>Content above</div>
+        <Divider />
+        <div>Content below</div>
+    </>
+);
