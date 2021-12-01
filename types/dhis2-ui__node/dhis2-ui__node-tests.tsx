04e41e91fa2b2751595a42db055373dbf52d15b2
Alexis Rico <sferadev@gmail.com>
2021-12-01 12:38:45 +0100

    Add node and sharing
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

---
+++ types/dhis2-ui__node/dhis2-ui__node-tests.tsx
@@ -0,0 +1,8 @@
+import { Node } from '@dhis2-ui/node';
+import * as React from 'react';
+
+const element = (
+    <Node component={<div>Component</div>} icon={<div>Icon</div>}>
+        Children
+    </Node>
+);
