50e3a37631cc5bc80f3a8d0e6a2e8af20661a615
Alexis Rico <sferadev@gmail.com>
2021-12-01 11:59:48 +0100

    Add cover and logo
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

---
+++ types/dhis2-ui__cover/dhis2-ui__cover-tests.tsx
@@ -0,0 +1,8 @@
+import { Cover } from '@dhis2-ui/cover';
+import * as React from 'react';
+
+const element = (
+    <Cover onClick={console.log}>
+        <button onClick={console.log}>Test</button>
+    </Cover>
+);
